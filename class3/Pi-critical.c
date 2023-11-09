31d8284ef9d7eff984c18d2735a2047f898c9e9e
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-11-09 18:31:24 +0100

    class-3 update

---
+++ class3/Pi-critical.c
@@ -0,0 +1,27 @@
+// Using the critical directive with pi so that the correct answer is generated
+
+// Faster do do with single thread with multiple sections
+
+#include <stdio.h>
+
+#define f(A) (4.0 / (1.0 + A * A))
+
+const long long n = 10000000000;
+
+int main(int argc, char *argv[]) {
+  long long i;
+  double x, sum, pi;
+  double w = 1.0 / n;
+  sum = 0.0;
+
+#pragma omp parallel for
+  for (i = 0; i < n; i++) {
+    x = w * ((double)i + 0.5);
+
+#pragma omp critical
+    sum = sum + f(x);
+  }
+
+  printf("pi = %.15f\n", w * sum);
+  return 0;
+}
---
+++ class3/pi-reduce.c
@@ -0,0 +1,23 @@
+// Using reduction to compute pi - works fine!
+
+#include <stdio.h>
+
+#define f(A) (4.0 / (1.0 + A * A))
+
+const long long n = 10000000000;
+
+int main(int argc, char *argv[]) {
+  long long i;
+  double x, sum, pi;
+  double w = 1.0 / n;
+  sum = 0.0;
+
+#pragma omp parallel for reduction(+ : sum)
+  for (i = 0; i < n; i++) {
+    x = w * ((double)i + 0.5);
+    sum = sum + f(x);
+  }
+
+  printf("pi = %.15f\n", w * sum);
+  return 0;
+}
