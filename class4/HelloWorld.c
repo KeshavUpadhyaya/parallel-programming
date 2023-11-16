6b3594381ff58efdef34ff1dd4a9f226b8e2972a
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-11-16 09:10:10 +0100

    class4

--- class4/HelloWorld.c
+++ class4/HelloWorld.c
@@ -2,15 +2,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// run with mpirun -n 4 --hostfile hostfile.txt --pernode ./a.out  for running
+// in 4 different machines
+
 int main(int argc, char **argv) {
 
-  int size = 0;
+  int size = 0, rank = 0;
   char hn[128];
   gethostname(hn, 128);
 
   // MUST BE CALLED at first
   MPI_Init(NULL, NULL);
-  printf("Hello World from %s \n", hn);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  printf("Hello World from %s. I am node %d of %d \n", hn, rank, size);
 
   // MUST BE CALLED at the end
   MPI_Finalize();
