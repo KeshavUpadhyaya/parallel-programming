444a2374b7f1f6e8415fe2cb9a92a7e58dd66cad
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-11-16 09:12:11 +0100

    class3 fib

---
+++ class3/fibonacci.c
@@ -0,0 +1,29 @@
+#include <omp.h>
+#include <stdio.h>
+
+int fibo(int n) {
+  int x, y;
+
+  if (n < 2)
+    return n;
+
+#pragma omp task shared(x) if (n > 30)
+  x = fibo(n - 1);
+
+#pragma omp task shared(y) if (n > 30)
+  y = fibo(n - 2);
+
+#pragma omp taskwait
+  return x + y;
+}
+
+int main() {
+  int NN = 10000;
+
+#pragma omp parallel
+  {
+
+#pragma omp master
+    fibo(NN);
+  }
+}
