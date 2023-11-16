5f92068ad7b82f9241caf09dd02288374611fa12
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-11-16 08:53:08 +0100

    update class 4

--- class4/HelloWorld.c
+++ class4/HelloWorld.c
@@ -1,13 +1,16 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 
   int size = 0;
+  char hn[128];
+  gethostname(hn, 128);
 
   // MUST BE CALLED at first
   MPI_Init(NULL, NULL);
-  printf("Hello World");
+  printf("Hello World from %s \n", hn);
 
   // MUST BE CALLED at the end
   MPI_Finalize();
