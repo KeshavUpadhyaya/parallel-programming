22ec25bbe9bdeb45e8120546698984c1f8e6852a
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-11-27 18:57:04 +0100

    added bandwidth test

--- class5-mpi/latency_bandwidth.c
+++ class5-mpi/latency_bandwidth.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TAG_PING 17
 #define TAG_PONG 23
@@ -27,6 +28,27 @@ void test_latency(int rank, int size, int N) {
   }
 }
 
+void test_bandwidth(int rank, int size, int message_size) {
+  double start_time, end_time;
+  MPI_Status status;
+
+  char *message = (char *)malloc(message_size);
+
+  if (rank == 0) {
+    start_time = MPI_Wtime();
+    MPI_Send(message, message_size, MPI_CHAR, 1, TAG_PING, MPI_COMM_WORLD);
+    end_time = MPI_Wtime();
+
+    double latency = (end_time - start_time) / 2 * 1E9;
+    double bandwidth = message_size / (end_time - start_time);
+    printf("Bandwidth: %lf MB/s\n", bandwidth / (1024 * 1024));
+
+  } else if (rank == 1) {
+    MPI_Recv(message, message_size, MPI_CHAR, 0, TAG_PING, MPI_COMM_WORLD,
+             &status);
+  }
+}
+
 int main(int argc, char const *argv[]) {
   MPI_Init(NULL, NULL);
 
@@ -36,6 +58,9 @@ int main(int argc, char const *argv[]) {
 
   int N = 1000;
   test_latency(rank, size, N);
+
+  int message_size = 1024 * 1024 * 10; // 10MB
+  bandwidth_test(rank, size, message_size);
 
   MPI_Finalize();
   return 0;
