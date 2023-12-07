0058fbc0c90c9e663d154bb7b6d2aa89e516c78c
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-12-07 10:46:03 +0100

    comm

---
+++ class6/comm.c
@@ -0,0 +1,18 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  MPI_Init(0, 0);
+
+  int wRank, wSize;
+  int sRank = -1, sSize = -1;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &wSize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &wRank);
+
+  printf("World Size: %d World rank : %d\n", wSize, wRank);
+  printf("Sub size: %d Sub rank: %d", &sSize, &sRank);
+
+  MPI_Finalize();
+  return 0;
+}
