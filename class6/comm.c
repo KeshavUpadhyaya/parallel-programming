8d6bbbc51d3cddcadbf65285058941bf7831e23f
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-12-07 10:54:24 +0100

    comm

--- class6/comm.c
+++ class6/comm.c
@@ -7,11 +7,16 @@ int main(int argc, char const *argv[]) {
   int wRank, wSize;
   int sRank = -1, sSize = -1;
 
+  MPI_Comm subComm;
+  int color = wRank % 3;
+  MPI_Comm_split(MPI_COMM_WORLD, color, 0, &subComm);
+
   MPI_Comm_size(MPI_COMM_WORLD, &wSize);
   MPI_Comm_rank(MPI_COMM_WORLD, &wRank);
 
-  printf("World Size: %d World rank : %d\n", wSize, wRank);
-  printf("Sub size: %d Sub rank: %d\n", sSize, sRank);
+  printf(
+      "World Size: %d World rank : %d Color: %d Sub size: %d Sub rank: %d \n",
+      wSize, wRank, color, sSize, sRank);
 
   MPI_Finalize();
   return 0;
